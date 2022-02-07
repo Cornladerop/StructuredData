eb524c999642b80a9eb6fd6d8e907f514d8b389e
Lin <99045139+Cornladerop@users.noreply.github.com>
2022-02-07 19:15:40 +0300

    Add simple implementation

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,1 +1,37 @@
+function Node(list, data) {
+    this.list = list;
+    this.data = data;
+    this.prev = null;
+    this.next = null;
+}
 
+class DoublyLinkedList {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    push(data) {
+        let node = new Node(this, data);
+        node.prev = this.last;
+        this.length ? this.last.next = node : this.first = node;
+        this.last = node;
+        this.length++;
+        return node;
+    }
+
+    pop() {
+        if (this.length === 0) return null;
+        let node = this.last;
+        node.list = null;
+        node.prev = null;
+        node.next = null;
+        this.length--;
+        return node.data;
+    }
+
+    size() {
+        return this.length
+    }
+}
