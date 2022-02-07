639cbd305e23ad27df8adc5bd57ad9eedfdaf833
Lin <99045139+Cornladerop@users.noreply.github.com>
2022-02-07 19:03:27 +0300

    Add simple implementation

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -1,1 +1,11 @@
-
+class SinglyLinkedList {
+    constructor() {
+        this.last = null;
+    }
+    push(data) {
+        let prev = this.last;
+        let el = {prev, data};
+        this.last = el;
+        return el;
+    }
+}
