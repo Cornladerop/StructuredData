e0411bd8600190e7faad97b77fcf3eb4ef7701f2
resufer <66846932+resufer@users.noreply.github.com>
2022-02-09 08:46:48 +0300

    Add pop and size

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -1,11 +1,28 @@
 class SinglyLinkedList {
     constructor() {
         this.last = null;
+        this.length = 0;
     }
+
     push(data) {
         let prev = this.last;
         let el = {prev, data};
         this.last = el;
+        this.length++;
         return el;
+    }
+
+    pop() {
+        if (this.length === 0) return null;
+        let node = this.last;
+        let data = node.data;
+        this.last = node.prev;
+        node = null;
+        this.length--;
+        return data;
+    }
+
+    size() {
+        return this.length
     }
 }
