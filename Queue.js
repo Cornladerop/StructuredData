11bcd45b62c066a28ce9e8cb193f7125ab236b8c
Lin <99045139+Cornladerop@users.noreply.github.com>
2022-02-09 09:35:36 +0300

    Add simple implementation

--- Queue.js
+++ Queue.js
@@ -1,1 +1,28 @@
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+    }
 
+    put(data) {
+        let last = this.last;
+        let el = {next: null, data};
+        if (last) {
+            last.next = el;
+            this.last = el;
+        } else {
+            this.first = el;
+            this.last = el;
+        }
+    }
+
+    pick() {
+        let el = this.first;
+        if (!el) return null;
+        if (this.last === el) {
+            this.first = null;
+            this.last = null;
+        } else this.first = el.next;
+        return el.data
+    }
+}
