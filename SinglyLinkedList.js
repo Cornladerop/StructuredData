b36ca89bc43431affda26900afb0c2b6f0e58709
resufer <66846932+resufer@users.noreply.github.com>
2022-02-11 22:33:07 +0300

    Add SinglyLinkedList on prototype

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -1,1 +1,26 @@
+function SinglyLinkedList() {
+    this.last = null;
+    this.length = 0;
+}
 
+SinglyLinkedList.prototype.push = function(data) {
+    let prev = this.last;
+    let el = {prev, data};
+    this.last = el;
+    this.length++;
+    return el;
+}
+
+SinglyLinkedList.prototype.pop = function() {
+    if (this.length === 0) return null;
+    let node = this.last;
+    let data = node.data;
+    this.last = node.prev;
+    node = null;
+    this.length--;
+    return data;
+}
+
+SinglyLinkedList.prototype.size = function() {
+    return this.length;
+}
