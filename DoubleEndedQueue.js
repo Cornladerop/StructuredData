cd306124adf6d24ae6a6cd0060dfe6536615de00
resufer <66846932+resufer@users.noreply.github.com>
2022-02-14 13:12:53 +0300

    Update DoubleEndedQueue.js

--- DoubleEndedQueue.js
+++ DoubleEndedQueue.js
@@ -1,63 +1,69 @@
-class Dequeue {
-    constructor() {
+function Dequeue() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+}
+
+Dequeue.prototype.push = function(data) {
+    let last = this.last;
+    let el = {prev: last, next: null, data};
+    if (last) {
+        last.next = el;
+        this.last = el;
+    } else {
+        this.first = el;
+        this.last = el;
+    }
+    this.length++;
+    return el;
+}
+
+Dequeue.prototype.pop = function() {
+    let el = this.last;
+    let res = el.data;
+    if (!el) return null;
+    if (this.first === el) {
         this.first = null;
         this.last = null;
-        this.length = 0;
+    } else {
+        this.last = el.prev;
+        el.prev.next = null;
+        el = null;
     }
+    this.length--;
+    return res;
+}
 
-    push(data) {
-        let last = this.last;
-        let el = {prev: last, next: null, data};
-        if (last) {
-            last.next = el;
-            this.last = el;
-        } else {
-            this.first = el;
-            this.last = el;
-        }
-        this.length++;
+Dequeue.prototype.unshift = function(data) {
+    let first = this.first;
+    let el = {prev: null, next: first, data}
+    if (first) {
+        first.prev = el;
+        this.first = el;
+    } else {
+        this.first = el;
+        this.last = el;
     }
+    this.length++;
+    return el;
+}
 
-    pop() {
-        let el = this.last;
-        if (!el) return null;
-        if (this.first === el) {
-            this.first = null;
-            this.last = null;
-        } else {
-            this.last = el.prev;
-        }
-        this.length--;
-        return el.data;
+Dequeue.prototype.shift = function() {
+    let el = this.first;
+    let res = el.data;
+    if (!el) return null;
+    if (this.last === el) {
+        this.first = null;
+        this.last = null;
+    } else {
+        this.first = el.next;
+        el.next.prev = null;
+        el = null;
     }
+    this.length--;
+    return res;
+}
 
-    unshift(data) {
-        let first = this.first;
-        let el = {prev: null, next: first, data}
-        if (first) {
-            first.prev = el;
-            this.first = el;
-        } else {
-            this.first = el;
-            this.last = el;
-        }
-        this.length++;
-    }
-
-    shift() {
-        let el = this.first;
-        if (!el) return null;
-        if (this.last === el) {
-            this.first = null;
-            this.last = null;
-        } else {
-            this.first = el.next;
-        }
-        this.length--;
-        return el.data;
-    }
-
-    size() {
-        return this.length
-    }
+Dequeue.prototype.size = function() {
+    return this.length;
 }
